9bb825e7cef0ecf3451c899e5c8ab6e36a0fd270
risottoinquiry <charles.olivier.fortin@gmail.com>
2022-10-08 13:21:11 -0400

    Add day 18

---
+++ day018.py
@@ -0,0 +1,21 @@
+attempts = 0
+number = 420
+print("NUMBER GUESSER! ! !")
+while True:
+  guess = int(input("What is your guess?: "))
+  if guess < 0:
+     exit()
+  if guess > number:
+    print("Too high")
+    attempts = attempts+1
+  elif guess < number:
+   print("Too low")
+   attempts = attempts+1
+  elif guess == number:
+   attempts = attempts+1
+   if attempts >1:
+     print("It took you",attempts,"guesses to get it correct!")
+     exit()
+   if attempts == 1:
+      print("It took you",attempts,"guess to get it correct!")
+      exit()
