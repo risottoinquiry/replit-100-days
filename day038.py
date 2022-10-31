e45bb23dc54613a9ea718b13973705a9d03f33a5
risottoinquiry <charles.olivier.fortin@gmail.com>
2022-10-30 20:30:44 -0400

    Add day 38

---
+++ day038.py
@@ -0,0 +1,18 @@
+def colorChange(color):
+   if color == "r":
+    print('\033[31m', end="")
+   elif color == "white":
+    print('\033[0m', end="")
+   elif color == "b":
+    print('\033[34m', end="")
+   elif color == "y":
+    print('\033[33m', end="")
+   elif color == "g":
+    print('\033[32m', end="")
+   elif color == "p":
+    print('\033[35m', end="")
+
+sentence = input("What sentence do you want to rainbow-ize(...)? > ")
+for letter in sentence:
+  colorChange(letter.lower())
+  print(letter,end="")
